8dbcfcfcdfb02ee49486753bc54ef7f469cd2baf
Andrada Lorena Popa <andrada.lorena.popa@gmail.com>
2019-11-14 17:29:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,9 +33,9 @@ class Song
   end
   def self.find_or_create_by_name(name)
     if name==name
-      self.find_by_name
+      self.find_by_name(name)
     else
-      self.create_by_name
+      self.create_by_name(name)
     end
   end
 end
